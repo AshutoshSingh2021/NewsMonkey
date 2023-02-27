d331a98904eb7aa0fe0cc3bfe41c9240b198ad6a
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2023-02-28 00:26:32 +0530

    Implementing react router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,75 +1,78 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            NewsMonkey
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link " aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  Business
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  Entertainment
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  General
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  Health
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  Science{" "}
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  Sports{" "}
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/About">
-                  Technology
-                </a>
-              </li>
-            </ul>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              NewsMonkey
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-Link " aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/business">
+                    Business
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/general">
+                    General
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/health">
+                    Health
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/science">
+                    Science
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/sports">
+                    Sports
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-Link" to="/technology">
+                    Technology
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     );
   }
 }
