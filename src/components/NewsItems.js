1536fc0f3f8c61196147f8081437e37ddba54171
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2023-03-06 21:46:06 +0530

    Added badges and author and timesection

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,14 +2,34 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <img src={imgUrl} className="card-img-top" alt="..." />
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "85%", zIndex: "1" }}
+          >
+            {source}
+          </span>
+          <img
+            src={
+              !imgUrl
+                ? " https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.23.3/images/bbc-sport-logo.png"
+                : imgUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on {new Date(date).toString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
